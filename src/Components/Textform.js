b2546a8df6fc128a26fbe496bc0a8d9cc2d1866d
AAYUSHI1507 <a.kataria@somaiya.edu>
2024-11-15 02:14:16 +0530

    updating the todays task !!! Yay!!!

--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -1,20 +1,45 @@
-import React from 'react'
+import React,{useState} from 'react'
 import PropTypes from 'prop-types'
-
 function Textform(props) {
+
+  const HandleOnChange = (event) =>{
+    console.log("On change");
+    setText(event.target.value);
+}
+  const HandleUpClick = () =>{
+      console.log("Converted to the Uppercase."+ text.toUpperCase());
+      //document.getElementById("myBox").value = text.toUpperCase();
+      if(text === ""){
+        let dflt= "This is the Default text5";
+        setText(dflt);
+        console.log("ITs in here "+ dflt);
+
+      }else{
+        setText(text.toUpperCase());
+      }
+  }
+  const RemoveTextOnClick = () =>{
+    console.log("Successfully removed the text");
+    // document.getElementById("myBox").value = "";
+    let newText = "";
+    setText(newText);
+}
+const [text, setText] = useState("This is the default contact");
+
   return (
-    
       <>
     <h2>{props.heading}</h2>
       <div className="mb-3">
         <textarea
           className="form-control"
           id="myBox"
+          onChange={HandleOnChange}
           rows="8"
+          value={text}
         ></textarea>
       </div>
-      <button className="btn btn-primary">Convert to Uppercase </button>
-      <button className="btn btn-dark ">Remove all text</button>
+      <button className="btn btn-primary my-2" onClick={HandleUpClick}>Convert to Uppercase </button>
+      <button className="btn btn-dark mx-2" onClick={RemoveTextOnClick}>Remove all text</button>
 
     </>
     
