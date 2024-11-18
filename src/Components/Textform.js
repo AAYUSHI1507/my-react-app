73db3b095eeb7b09e7c37d20d4a308b6490f54d7
AAYUSHI1507 <a.kataria@somaiya.edu>
2024-11-19 02:41:10 +0530

    Damm again 2nd time updating but done little bit.

--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -41,37 +41,47 @@ function Textform(props) {
     // setText(newText);
     let textarr = text.split(" ");
     let arr2 = "";
-    try{
-    for (let i = 0; i < textarr.length; i++) {
-      let wordarr = textarr[i][0].toUpperCase()+textarr[i].slice(1,textarr[i].length).toLowerCase();
-      arr2 = arr2+" "+wordarr;
+    try {
+      for (let i = 0; i < textarr.length; i++) {
+        let wordarr =
+          textarr[i][0].toUpperCase() +
+          textarr[i].slice(1, textarr[i].length).toLowerCase();
+        arr2 = arr2 + " " + wordarr;
+      }
+      setText(arr2);
+    } catch (Exception) {
+      console.log("Here is the execption occured.");
+      setText("Exception occured");
     }
-    setText(arr2);
-  } 
-  catch(Exception ){
-    console.log("Here is the execption occured.");
-    setText("Exception occured");
-  }
   };
-  const HandleCopy = ()=>{
+  const HandleCopy = () => {
     let text = document.getElementById("myBox");
     text.select();
     navigator.clipboard.writeText(text.value);
-  }
-  const handleExtraSpaces =()=>{
+  };
+  const handleExtraSpaces = () => {
     let newText = text.split(/[ ]+/);
     setText(newText.join(" "));
-  }
+  };
   const [text, setText] = useState("");
 
   return (
     <>
-      <div className="container">
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "whitesmoke" : "black" }}
+      >
         <h2>{props.heading}</h2>
-        <div className="mb-3">
+        <div
+          className="mb-3"
+          style={{ color: props.mode === "dark" ? "whitesmoke" : "black" }}
+        >
           <textarea
             className="form-control"
-            style={{backgroundColor: props.mode === "light"?"grey":"#32363a"}}
+            style={{
+              backgroundColor: props.mode === "light" ? "#e1e1e1" : "#32363a",
+              color: props.mode === "dark" ? "whitesmoke" : "black",
+            }}
             id="myBox"
             onChange={HandleOnChange}
             rows="8"
@@ -87,22 +97,30 @@ function Textform(props) {
         <button className="btn btn-success my-2" onClick={HandleDownClick}>
           Convert to Lowercase{" "}
         </button>
-        <button className="btn btn-primary my-2 mx-2" onClick={HandleUpDownClick}>
+        <button
+          className="btn btn-primary my-2 mx-2"
+          onClick={HandleUpDownClick}
+        >
           Convert to Capitalize
         </button>
         <button className="btn btn-primary my-2 mx-2" onClick={HandleCopy}>
           Copy Text
         </button>
-        <button className="btn btn-primary my-2 mx-2" onClick={handleExtraSpaces}>
+        <button
+          className="btn btn-primary my-2 mx-2"
+          onClick={handleExtraSpaces}
+        >
           Remove Extra Spaces
         </button>
       </div>
-      <div className="container my-2">
+      <div className="container my-2"style={{ color: props.mode === "dark" ? "whitesmoke" : "black" }}>
         <h1>Your text summary</h1>
-        <p>There are {text.length} and {text.split(" ").length} words</p>
-        
+        <p>
+          There are {text.length} and {text.split(" ").length} words
+        </p>
+
         <h2>How much time does it takes to read slowly.</h2>
-        <p>It takes {0.008*(text.split(" ").length)} minute</p>
+        <p>It takes {0.008 * text.split(" ").length} minute</p>
 
         <h3>Preview</h3>
         <p>{text}</p>
